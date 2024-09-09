b64bbe57058b4ae68166842b3540dbc16f9bae5b
Dan Zango <danz@wix.com>
2024-09-09 18:56:42 +0300

    first commit

---
+++ 01-starting-project/src/App.jsx
@@ -0,0 +1,24 @@
+import { CORE_CONCEPTS } from './data.js';
+import CoreConcept from './components/CoreConcepts.jsx';
+import Header from './components/Header.jsx';
+
+function App() {
+  return (
+    <div>
+      <Header/> 
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
