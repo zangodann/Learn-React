62796f96edb9d38d65356eeed5acba4865773791
Dan Zango <danz@wix.com>
2024-09-10 12:10:18 +0300

    added TabButton component and tab content

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,12 +1,24 @@
-import { CORE_CONCEPTS } from './data.js';
+import { useState } from 'react';
+import { CORE_CONCEPTS , EXAMPLES} from './data.js';
 import CoreConcept from './components/CoreConcepts.jsx';
 import Header from './components/Header/Header.jsx';
+import TabButton from './components/TabButton.jsx';
+
+// npm run dev
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState('components');
+
+  function handleSelect(selectedButton){
+    // selectedButton => 'Components', 'JSX', 'Props', 'State' 
+    setSelectedTopic(selectedButton);
+  }
+
   return (
     <div>
       <Header/> 
       <main>
+
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
@@ -15,6 +27,23 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+          </menu>
+          <div id='tab-content'>
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
